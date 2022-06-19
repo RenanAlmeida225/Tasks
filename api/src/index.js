05869334020b52852147382ea089ac722e602ca9
Italo Renan <italorenan225@gmail.com>
2022-06-19 06:13:06 -0300

    added new rotes for task

--- api/src/index.js
+++ api/src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const conn = require('./db/conn.js');
 const AuthRoutes = require('./routes/AuthRoutes.js');
-//const TasksRoutes = require('./routes/TasksRoutes.js');
+const TasksRoutes = require('./routes/TasksRoutes.js');
 
 const port = process.env.PORT || 5000;
 
@@ -13,8 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/api/auth', AuthRoutes);
-//app.use('/api/tasks', TasksRoutes);
+app.use('/api/tasks', TasksRoutes);
 
 conn.sync({ force: false })
 	.then(app.listen(port, () => console.log(`Open on port ${port}!`)))
-	.catch(error => console.error(error));
+	.catch((error) => console.error(error));
