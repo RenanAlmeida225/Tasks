fd61b22e614e4a8a6f82aac283959d72957e640c
Italo Renan <italorenan225@gmail.com>
2022-06-19 06:15:53 -0300

    added new function for find all tasks

--- api/src/controllers/TasksCrontroller.js
+++ api/src/controllers/TasksCrontroller.js
@@ -1,26 +1,31 @@
 const Tasks = require('../models/Tasks.js');
-const Objectives = require('../models/Objectives.js');
 
-const TasksController = {
-	async allTasks(req, res) {
-		res.status(200).json({
-			message: 'ok',
-		});
-	},
+class TasksController {
+	static async allTasks(req, res) {
+		const { userId } = req.body;
+		try {
+			const tasks = await Tasks.findAll({ where: { userId: userId } });
+			return res.status(200).json({
+				tasks
+			});
+		} catch (error) {
+			return res.status(400).json({ error: error.message });
+		}
+	}
 
-	async createTasks(req, res) {
+	static async createTasks(req, res) {
 		const userId = req.params.userId;
 		const title = req.body.title;
 
 		if (!userId) {
 			return res.status(422).json({
-				error: 'User id is requeired!',
+				error: 'User id is requeired!'
 			});
 		}
 
 		if (!title) {
 			return res.status(422).json({
-				error: 'Title is requeired!',
+				error: 'Title is requeired!'
 			});
 		}
 
@@ -29,15 +34,15 @@ const TasksController = {
 
 			return res.status(201).json({
 				message: 'User created successfullyr!',
-				tasks,
+				tasks
 			});
 		} catch (error) {
 			console.log(error);
 			return res.status(500).json({
-				error: error,
+				error: error
 			});
 		}
-	},
+	}
 
 	async createObjectives(req, res) {
 		const tasksId = req.params.tasksId;
@@ -45,32 +50,32 @@ const TasksController = {
 
 		if (!tasksId) {
 			res.status(422).json({
-				error: 'Tasks id is requeired!',
+				error: 'Tasks id is requeired!'
 			});
 		}
 
 		if (!contents) {
 			res.status(422).json({
-				error: 'Contents is requeired!',
+				error: 'Contents is requeired!'
 			});
 		}
 
 		try {
 			const objectives = await Objectives.create({
 				contents: contents,
-				tasksId: tasksId,
+				tasksId: tasksId
 			});
 
 			return res.status(201).json({
 				message: 'User created successfullyr!',
-				objectives,
+				objectives
 			});
 		} catch (error) {
 			return res.status(500).json({
-				error: error,
+				error: error
 			});
 		}
-	},
-};
+	}
+}
 
 module.exports = TasksController;
