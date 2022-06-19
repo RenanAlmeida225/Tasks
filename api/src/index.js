32d0a35f2eaa470f5f2bfe12fd49443057b68a72
Italo Renan <italorenan225@gmail.com>
2022-06-19 05:47:28 -0300

    remove cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,32 +1,20 @@
 const express = require('express');
-const conn = require('./db/conn.js');
-const cors = require('cors');
 require('dotenv').config();
 
-// Importando as rotas
+const conn = require('./db/conn.js');
 const AuthRoutes = require('./routes/AuthRoutes.js');
-const TasksRoutes = require('./routes/TasksRoutes.js');
+//const TasksRoutes = require('./routes/TasksRoutes.js');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-// Configurando o Cors
-app.use(
-	cors({
-		origin: process.env.URL_CLIENT || 'http://localhost:3000',
-		optionsSuccessStatus: 200,
-	})
-);
-
-// Configurando o json
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/api/authentication', AuthRoutes);
-app.use('/api/tasks', TasksRoutes);
+app.use('/api/auth', AuthRoutes);
+//app.use('/api/tasks', TasksRoutes);
 
-//{ alter: true }{ force: true }
 conn.sync({ force: false })
 	.then(app.listen(port, () => console.log(`Open on port ${port}!`)))
 	.catch(error => console.error(error));
