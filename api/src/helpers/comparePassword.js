789f388bdbf41549fcb792dbd9944ae3b8c6e3f2
Italo Renan <italorenan225@gmail.com>
2022-06-19 05:45:55 -0300

    Added new function for compare password

---
+++ api/src/helpers/comparePassword.js
@@ -0,0 +1,5 @@
+const { compareSync } = require('bcryptjs');
+
+module.exports = function comparePassword(password, hash) {
+	if (!compareSync(password, hash)) throw new Error('Password incorret!');
+};
