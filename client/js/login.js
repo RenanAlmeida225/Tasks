ee4b60f1aa8d73294361c7247673d82b691d36cd
Italo Renan <italorenan225@gmail.com>
2022-06-21 11:45:48 -0300

    changed file login

--- client/js/login.js
+++ client/js/login.js
@@ -1,23 +1,20 @@
 const formlogin = document.querySelector('.form-login');
 const formRegister = document.querySelector('.form-register');
 
+const userName = document.querySelector('#user-name').value;
+const email = document.querySelector('#email').value;
+const password = document.querySelector('#password').value;
+const confirmPassword = document.querySelector('#confirm-password').value;
+
 formRegister.addEventListener('submit', handleSubmitregister);
 formlogin.addEventListener('submit', handleSubmitLogin);
 
 function handleSubmitregister(event) {
 	event.preventDefault();
-	const email = document.querySelector('#email').value;
-	const password = document.querySelector('#password').value;
-
-	console.log(`emial = ${email}, password = ${password}`);
 	window.location.assign('./tasks/tasks.html');
 }
 
 function handleSubmitLogin(event) {
 	event.preventDefault();
-	const email = document.querySelector('#email').value;
-	const password = document.querySelector('#password').value;
-
-	console.log(`emial = ${email}, password = ${password}`);
 	window.location.assign('./tasks/tasks.html');
 }
