63f5f24afb7c893034e58fc406b9693581c9c7ea
Italo Renan <italorenan225@gmail.com>
2022-06-25 14:00:48 -0300

    Added verify auth in routes

--- api/src/routes/TasksRoutes.js
+++ api/src/routes/TasksRoutes.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 
 const TasksController = require('../controllers/TasksCrontroller.js');
+const verifyToken = require('../helpers/verifyToken');
 
 router
 	.route('/')
-	.get(TasksController.allTasks)
-	.post(TasksController.createTasks);
+	.get(verifyToken, TasksController.allTasks)
+	.post(verifyToken, TasksController.createTasks);
 
 router
 	.route('/:id')
-	.get(TasksController.getTasksById)
-	.put(TasksController.updateTasks)
-	.delete(TasksController.deleteTasks);
+	.get(verifyToken, TasksController.getTasksById)
+	.put(verifyToken, TasksController.updateTasks)
+	.delete(verifyToken, TasksController.deleteTasks);
 
 module.exports = router;
