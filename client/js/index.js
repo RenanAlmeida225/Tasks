950e287cdb7a09d893ecb9db0ed0eef3f3a3da4e
Italo Renan <italorenan225@gmail.com>
2022-06-27 09:23:08 -0300

    Added option delete tasks

--- client/js/index.js
+++ client/js/index.js
@@ -1,16 +1,92 @@
 const tasks = document.querySelector('.tasks');
-const createButton = document.querySelector('.icon-create');
+const createModal = document.querySelector('.icon-create');
+const formModal = document.querySelector('.form-modal');
 
-createButton.addEventListener('click', () => {
-	createTasks(tasks);
+fetch('http://localhost:5000/api/tasks', {
+	method: 'GET',
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json',
+		authorization: `Bearer ${localStorage.getItem('token')}`
+	}
+})
+	.then((res) => {
+		return res.json();
+	})
+	.then((data) => {
+		const dataTasks = data.tasks;
+
+		dataTasks.map((task) => {
+			createTasks(tasks, task.title, task.id);
+		});
+	});
+
+createModal.addEventListener('click', () => {
+	const modal = document.querySelector('.modal-container');
+
+	modal.style.display = 'flex';
+	modal.addEventListener('click', ({ target }) => {
+		if (
+			target.className === 'close' ||
+			target.className === 'modal-container'
+		) {
+			modal.style.display = 'none';
+		}
+	});
+	// fetch('http://localhost:5000/api/tasks', {
+	// 	method: 'POST',
+	// 	headers: {
+	// 		Accept: 'application/json',
+	// 		'Content-Type': 'application/json',
+	// 		authorization: `Bearer ${localStorage.getItem('token')}`
+	// 	},
+	// 	body: JSON.stringify({ title: 'title' })
+	// }).then((res) => {
+	// 	console.log(res.json());
+	// });
+	//createTasks(tasks);
 	//window.location.assign('./teste.html');
 });
 
-function createTasks(element) {
-	const node =
-		'<div class="task">\n\
+formModal.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const title = document.querySelector('#title-modal');
+
+	if (title.value !== '') {
+		fetch('http://localhost:5000/api/tasks', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				authorization: `Bearer ${localStorage.getItem('token')}`
+			},
+			body: JSON.stringify({ title: title.value })
+		}).then((res) => {
+			return res.json();
+		});
+
+		title.value = '';
+	}
+});
+
+function deleteTask(taskId) {
+	console.log(taskId);
+	fetch(`http://localhost:5000/api/tasks/${taskId}`, {
+		method: 'DELETE',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			authorization: `Bearer ${localStorage.getItem('token')}`
+		}
+	}).then((res) => {
+		return res.json();
+	});
+}
+
+function createTasks(element, title, taskId) {
+	const node = `<div class="task">\n\
     <div class="title">\n\
-        <span>Teste de tasks</span>\n\
+        <span>${title}</span>\n\
     </div>\n\
     <div class="icons">\n\
         <div class="icon icon-check">\n\
@@ -25,11 +101,11 @@ function createTasks(element) {
             </button>\n\
         </div>\n\
         <div class="icon icon-delete">\n\
-            <button>\n\
+            <button onclick="deleteTask(${taskId})">\n\
                 <i class="bi bi-trash"></i>\n\
             </button>\n\
         </div>\n\
     </div>\n\
-</div>';
+</div>`;
 	element.insertAdjacentHTML('beforeend', node);
 }
--- client/js/login.js
+++ client/js/login.js
@@ -1,11 +1,6 @@
 const formlogin = document.querySelector('.form-login');
 const formRegister = document.querySelector('.form-register');
 
-const userName = document.querySelector('#user-name').value;
-const email = document.querySelector('#email').value;
-const password = document.querySelector('#password').value;
-const confirmPassword = document.querySelector('#confirm-password').value;
-
 formRegister.addEventListener('submit', handleSubmitregister);
 formlogin.addEventListener('submit', handleSubmitLogin);
 
@@ -16,5 +11,25 @@ function handleSubmitregister(event) {
 
 function handleSubmitLogin(event) {
 	event.preventDefault();
-	window.location.assign('./tasks/tasks.html');
+	const email = document.querySelector('#email-login').value;
+	const password = document.querySelector('#password-login').value;
+
+	fetch('http://localhost:5000/api/auth/login', {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ email: email, password: password })
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			if (data.token) {
+				localStorage.setItem('token', data.token);
+				location.assign('./tasks/tasks.html');
+			}
+		})
+		.catch((error) => console.error(error));
 }
