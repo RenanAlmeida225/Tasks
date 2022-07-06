bcd807d78192bce01eabf20cbe26ef2d8f60c02b
Italo Renan <italorenan225@gmail.com>
2022-07-06 08:48:57 -0300

    Added function for complete task

--- client/js/index.js
+++ client/js/index.js
@@ -2,26 +2,52 @@ const tasks = document.querySelector('.tasks');
 const createModal = document.querySelector('.icon-create');
 const formModal = document.querySelector('.form-modal');
 
-fetch('http://localhost:5000/api/tasks', {
-	method: 'GET',
-	headers: {
-		Accept: 'application/json',
-		'Content-Type': 'application/json',
-		authorization: `Bearer ${localStorage.getItem('token')}`
-	}
-})
-	.then((res) => {
-		return res.json();
+window.addEventListener('load', () => {
+	fetch('http://localhost:5000/api/tasks', {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			authorization: `Bearer ${localStorage.getItem('token')}`
+		}
 	})
-	.then((data) => {
-		const dataTasks = data.tasks;
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			const dataTasks = data.tasks;
 
-		dataTasks.map((task) => {
-			createTasks(tasks, task.title, task.id);
+			dataTasks.map((task) => {
+				createTasks(tasks, task.title, task.id, task.complete);
+			});
 		});
-	});
+});
 
 createModal.addEventListener('click', () => {
+	const title = document.querySelector('#title-modal');
+	title.value = '';
+	openModel();
+	formModal.addEventListener('submit', (event) => {
+		event.preventDefault();
+		if (title.value !== '') {
+			fetch('http://localhost:5000/api/tasks', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+					authorization: `Bearer ${localStorage.getItem('token')}`
+				},
+				body: JSON.stringify({ title: title.value })
+			}).then((res) => {
+				return res.json();
+			});
+
+			title.value = '';
+		}
+	});
+});
+
+function openModel() {
 	const modal = document.querySelector('.modal-container');
 
 	modal.style.display = 'flex';
@@ -33,44 +59,32 @@ createModal.addEventListener('click', () => {
 			modal.style.display = 'none';
 		}
 	});
-	// fetch('http://localhost:5000/api/tasks', {
-	// 	method: 'POST',
-	// 	headers: {
-	// 		Accept: 'application/json',
-	// 		'Content-Type': 'application/json',
-	// 		authorization: `Bearer ${localStorage.getItem('token')}`
-	// 	},
-	// 	body: JSON.stringify({ title: 'title' })
-	// }).then((res) => {
-	// 	console.log(res.json());
-	// });
-	//createTasks(tasks);
-	//window.location.assign('./teste.html');
-});
+}
 
-formModal.addEventListener('submit', (event) => {
-	event.preventDefault();
-	const title = document.querySelector('#title-modal');
+function editTask(title, taskId) {
+	openModel();
+	const titleModal = document.querySelector('#title-modal');
+	titleModal.value = title;
 
-	if (title.value !== '') {
-		fetch('http://localhost:5000/api/tasks', {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-				authorization: `Bearer ${localStorage.getItem('token')}`
-			},
-			body: JSON.stringify({ title: title.value })
-		}).then((res) => {
-			return res.json();
-		});
-
-		title.value = '';
-	}
-});
+	formModal.addEventListener('submit', (event) => {
+		event.preventDefault();
+		if (titleModal.value !== '') {
+			fetch(`http://localhost:5000/api/tasks/${taskId}`, {
+				method: 'PUT',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+					authorization: `Bearer ${localStorage.getItem('token')}`
+				},
+				body: JSON.stringify({ title: titleModal.value })
+			}).then((res) => {
+				return res.json();
+			});
+		}
+	});
+}
 
 function deleteTask(taskId) {
-	console.log(taskId);
 	fetch(`http://localhost:5000/api/tasks/${taskId}`, {
 		method: 'DELETE',
 		headers: {
@@ -83,20 +97,43 @@ function deleteTask(taskId) {
 	});
 }
 
-function createTasks(element, title, taskId) {
-	const node = `<div class="task">\n\
+function completeTasks(taskId) {
+	fetch(`http://localhost:5000/api/tasks/complete/${taskId}`, {
+		method: 'PUT',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			authorization: `Bearer ${localStorage.getItem('token')}`
+		}
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			console.log(data.complete);
+		});
+}
+
+function createTasks(element, title, taskId, complete) {
+	let iscomplete = complete
+		? '<div class="task complete">'
+		: '<div class="task">';
+
+	let boxCheck = complete ? 'check-square' : 'square';
+
+	console.log('==>', complete, '\n==>', iscomplete);
+	const node = `${iscomplete}\n\
     <div class="title">\n\
         <span>${title}</span>\n\
     </div>\n\
     <div class="icons">\n\
         <div class="icon icon-check">\n\
-            <button>\n\
-                <i class="bi bi-check-square"></i>\n\
-                <!-- <i class="bi bi-square"></i> -->\n\
+            <button onclick = "completeTasks(${taskId})">\n\
+				<i class="bi bi-${boxCheck}"></i>\n\
             </button>\n\
         </div>\n\
         <div class="icon icon-edit">\n\
-            <button>\n\
+            <button onclick="editTask('${title}', ${taskId})">\n\
                 <i class="bi bi-pencil-square"></i>\n\
             </button>\n\
         </div>\n\
