8406c7c6331ae008e5d908d6ef8dced1a0812bba
Italo Renan <italorenan225@gmail.com>
2022-07-14 12:14:44 -0300

    Added file to config jest

--- src/helpers/comparePassword.js
+++ src/helpers/comparePassword.js
@@ -1,4 +1,4 @@
-const { compareSync } = require('bcryptjs');
+const {compareSync} = require('bcryptjs');
 
 module.exports = function comparePassword(password, hash) {
 	if (!compareSync(password, hash)) throw new Error('Password incorret!');
